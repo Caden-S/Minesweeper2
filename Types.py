8ffc70b8bcd1415acf6f4476a8cbd945f7967380
Caden Summers <caden7700@gmail.com>
2022-02-01 22:19:53 -0500

    cleaned up functions, added safe area for on-click

--- Types.py
+++ Types.py
@@ -1,34 +1,36 @@
-import random
+from random import randrange
+from math import ceil
 
 class Game:
     def play(self):
-        difficulty = int(input("Select the difficulty (1-3): "))
-        difficulty -= 1
-        board_size = self.get_board_size(difficulty)
-
+        difficulty = int(input("Select the difficulty (1-3): ")) - 1
+        board_x = self.get_board_rows(difficulty)
+        board_y = self.get_board_cols(difficulty)
+        board_size = [board_x, board_y]
+        safe_area = self.get_safe_area(board_size)
         board = self.create_board(board_size, difficulty)
-
         resp = self.bomb_loop(difficulty, board, board_size)
         board = resp[0]
         bomb_list = resp[1]
 
         self.set_bomb_counter(board, bomb_list)
         self.board_print(board)
-        self.set_safe_area(bomb_list)
+        #self.set_safe_area(bomb_list)
 
     def board_print(self, board):
         for row in board:
             print(row)
 
     def create_board(self, board_size, difficulty):
-        if difficulty == 2:
-            return [ [0 for x in range(0, 30)] for y in range(0, 16) ]
-        else:
-            return [ [0 for x in range(0, board_size)] for y in range(0, board_size)]
+            return [ [0 for x in range(0, board_size[1])] for y in range(0, board_size[0]) ]
 
-    def get_board_size(self, difficulty):
+    def get_board_rows(self, difficulty):
         sizes = [[8,9,10], [13,14,15], [16,16,16]]
-        return sizes[difficulty][random.randrange(0,3,1)]
+        return sizes[difficulty][randrange(0,3)]
+    
+    def get_board_cols(self, difficulty):
+        sizes = [[8,9,10], [13,14,15], [30,30,30]]
+        return sizes[difficulty][randrange(0,3)]
 
     def get_bomb_count(self, difficulty):
         bomb_nums = [10,40,99]
@@ -43,16 +45,14 @@ class Game:
         return [board, bomb_list]
 
     def place_bomb(self, board, board_size, difficulty, bomb_list):
-        row = random.randrange(0, board_size - 1, 1)
-        col = random.randrange(0, board_size - 1, 1)
-        row16 = random.randrange(0, 15, 1)
-        col30 = random.randrange(0, 29, 1)
+        row = randrange(0, board_size[0] - 1, 1)
+        col = randrange(0, board_size[1] - 1, 1)
+        safe_area = self.get_safe_area(board_size)
 
-        if [row, col] in bomb_list or [row16, col30] in bomb_list:
+        if [row, col] in bomb_list:
             return self.place_bomb(board, board_size, difficulty, bomb_list)
-
-        if difficulty == 2:
-            return [row16, col30]
+        if [row, col] in safe_area:
+            return self.place_bomb(board, board_size, difficulty, bomb_list)
         else:
             return [row, col]
     
@@ -69,12 +69,13 @@ class Game:
                             continue
         return board 
 
-    def set_safe_area(self, bomb_list):
+    def get_safe_area(self, board_size):
         # set 3x3 box in bomb_list to prevent bombs in click area
-        # bomb_location = x get bomb location from click
-        bomb_row1 = [[-1, -1], [-1, 0], [-1, 1]]
-        bomb_row2 = [[0, -1], [0, 0], [0, 1]]
-        bomb_row3 = [[1, -1], [1, 0], [1, 1]]
+        click_row = ceil(board_size[0] / 2) - 1
+        click_col = ceil(board_size[1] / 2) - 1
+        bomb_row1 = [[click_row-1, click_col-1], [click_row-1, click_col], [click_row-1, click_col+1]]
+        bomb_row2 = [[click_row, click_col-1], [click_row, click_col], [click_row, click_col+1]]
+        bomb_row3 = [[click_row+1, click_col-1], [click_row+1, click_col], [click_row+1, click_col+1]]
         bomb_row1 += bomb_row2
         bomb_row1 += bomb_row3
-        print(bomb_row1)
+        return bomb_row1
