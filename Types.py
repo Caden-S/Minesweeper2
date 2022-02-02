343f2fc75c91158dbe278081ec9e64e5972d373c
Caden-S <caden7700@gmail.com>
2022-02-02 08:44:20 -0500

    Changed safe_area function so it's no longer hard-coded. set_bomb_counter now only check tiles adjacent to them instead of all

--- Types.py
+++ Types.py
@@ -4,16 +4,18 @@ from math import ceil
 class Game:
     def play(self):
         difficulty = int(input("Select the difficulty (1-3): ")) - 1
-        board_x = self.get_board_rows(difficulty)
-        board_y = self.get_board_cols(difficulty)
-        board_size = [board_x, board_y]
+
+        board_rows = self.get_board_rows(difficulty)
+        board_cols = self.get_board_cols(difficulty)
+        board_size = [board_rows, board_cols]
         safe_area = self.get_safe_area(board_size)
+
         board = self.create_board(board_size, difficulty)
         resp = self.bomb_loop(difficulty, board, board_size)
+
         board = resp[0]
         bomb_list = resp[1]
 
-        self.set_bomb_counter(board, bomb_list)
         self.board_print(board)
         #self.set_safe_area(bomb_list)
 
@@ -22,7 +24,8 @@ class Game:
             print(row)
 
     def create_board(self, board_size, difficulty):
-            return [ [0 for x in range(0, board_size[1])] for y in range(0, board_size[0]) ]
+        # returns board with x rows and y columns
+        return [ [0 for x in range(0, board_size[1])] for y in range(0, board_size[0]) ]
 
     def get_board_rows(self, difficulty):
         sizes = [[8,9,10], [13,14,15], [16,16,16]]
@@ -37,6 +40,7 @@ class Game:
         return bomb_nums[difficulty]
     
     def bomb_loop(self, difficulty, board, board_size):
+        # loop for placing bombs
         bomb_list = []
         for bomb in range(0, self.get_bomb_count(difficulty)):
             bomb_loc = self.place_bomb(board, board_size, difficulty, bomb_list)
@@ -45,6 +49,7 @@ class Game:
         return [board, bomb_list]
 
     def place_bomb(self, board, board_size, difficulty, bomb_list):
+        # places bombs anywhere outside the safe area and the already existing bomb list
         row = randrange(0, board_size[0] - 1, 1)
         col = randrange(0, board_size[1] - 1, 1)
         safe_area = self.get_safe_area(board_size)
@@ -54,28 +59,25 @@ class Game:
         if [row, col] in safe_area:
             return self.place_bomb(board, board_size, difficulty, bomb_list)
         else:
+            self.set_bomb_counter(board, row, col)
             return [row, col]
     
-    def set_bomb_counter(self, board, bomb_list):
-        for row in range(0, len(board)):
-            for tile in range(0, len(board[0])):
-                if [row, tile] in bomb_list:
+    def set_bomb_counter(self, board, row, col):
+        # sets number of bombs surrounding each tile
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if board[row + i][col + j] == 9:
                     continue
                 else:
-                    for bomb in bomb_list:
-                        if abs(bomb[0] - row) <= 1 and abs(bomb[1] - tile) <= 1:
-                            board[row][tile] += 1
-                        else:
-                            continue
-        return board 
+                    board[row +i][col + j] += 1
 
     def get_safe_area(self, board_size):
         # set 3x3 box in bomb_list to prevent bombs in click area
+        # change later to use click location
         click_row = ceil(board_size[0] / 2) - 1
         click_col = ceil(board_size[1] / 2) - 1
-        bomb_row1 = [[click_row-1, click_col-1], [click_row-1, click_col], [click_row-1, click_col+1]]
-        bomb_row2 = [[click_row, click_col-1], [click_row, click_col], [click_row, click_col+1]]
-        bomb_row3 = [[click_row+1, click_col-1], [click_row+1, click_col], [click_row+1, click_col+1]]
-        bomb_row1 += bomb_row2
-        bomb_row1 += bomb_row3
-        return bomb_row1
+        bomb_rows = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                bomb_rows += [[click_row + i, click_col + j]]
+        return bomb_rows
